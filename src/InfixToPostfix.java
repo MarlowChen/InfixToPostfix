c0802eb77890604012f5232bc80f9609b8b743a6
MarlowChen <mac@madphilosophy.idv.tw>
2017-02-28 16:58:18 +0800

    [New] Add Infix to postfix , set infix, postfix  and Stack

---
+++ src/InfixToPostfix.java
@@ -0,0 +1,15 @@
+
+
+import java.util.Stack;
+
+public class InfixToPostfix {
+
+	public static void main(String[] args) {
+		
+		String infix = "1+2";
+		String postfix = "";
+		Stack<Character> javaStack = new Stack<Character>(); 
+
+	}
+
+}
