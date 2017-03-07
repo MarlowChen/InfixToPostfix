f886cb036d075e4b0aa55e08511e58c1aec3ac03
MarlowChen <mac@madphilosophy.idv.tw>
2017-03-07 22:50:50 +0800

    [Modify] Add Addition Subtraction to Stack

--- src/InfixToPostfix.java
+++ src/InfixToPostfix.java
@@ -6,10 +6,42 @@ public class InfixToPostfix {
 
 	public static void main(String[] args) {
 		
-		String infix = "1+2";
+		String infix = "1+2+3-4+8";
 		String postfix = "";
 		Stack<Character> javaStack = new Stack<Character>(); 
 
+		//set infix to charArray and get every character
+		
+		for(char ch: infix.toCharArray()){
+			
+			switch (ch) {
+			case '+':
+			case '-':	
+				while(!javaStack.isEmpty()){
+					char topch = javaStack.pop();
+					
+					//  + or - == + or - so, print  topch
+ 					if(topch == '+' || topch == '-'){
+ 						postfix = postfix + topch;
+					}
+				}
+				//push this ch
+				
+				javaStack.push(ch);
+				break;
+
+			default: postfix = postfix + ch ;
+				break;
+			}
+			
+			
+		}
+		
+		while(!javaStack.isEmpty()){
+			postfix = postfix + javaStack.pop();
+		}
+		
+		System.out.println(postfix);
 	}
 
 }
