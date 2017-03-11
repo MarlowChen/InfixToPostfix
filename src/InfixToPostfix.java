45f6963a80ccd6538e6d035c4277ec6f779e9367
MarlowChen <mac@madphilosophy.idv.tw>
2017-03-11 10:07:37 +0800

    [Modify] Add Multiplied Divided to Stack

--- src/InfixToPostfix.java
+++ src/InfixToPostfix.java
@@ -6,7 +6,7 @@ public class InfixToPostfix {
 
 	public static void main(String[] args) {
 		
-		String infix = "1+2+3-4+8";
+		String infix = "1+2*3-4/8";
 		String postfix = "";
 		Stack<Character> javaStack = new Stack<Character>(); 
 
@@ -19,17 +19,42 @@ public class InfixToPostfix {
 			case '-':	
 				while(!javaStack.isEmpty()){
 					char topch = javaStack.pop();
-					
+					//get stack's top and comparison with + or - 
 					//  + or - == + or - so, print  topch
  					if(topch == '+' || topch == '-'){
  						postfix = postfix + topch;
+ 					
+ 					// * or / > + or - so,print  topch	
+					}else if(topch == '*' || topch == '/'){
+						postfix = postfix + topch;
 					}
 				}
 				//push this ch
 				
 				javaStack.push(ch);
 				break;
-
+				
+			case '*':
+			case '/':
+				
+				while(!javaStack.isEmpty()){
+					char topch = javaStack.pop();
+					//get stack's top and comparison with * or / 
+					//  * or / == * or / so, print  topch
+ 					if(topch == '*' || topch == '/'){
+ 						postfix = postfix + topch;
+ 						
+ 					// * or / > + or - so,push back to stack	
+					}else if(topch == '+' || topch == '-'){
+						javaStack.push(topch);
+						break;
+					}
+				}
+				//push this ch
+				
+				javaStack.push(ch);
+				break;	
+				
 			default: postfix = postfix + ch ;
 				break;
 			}
